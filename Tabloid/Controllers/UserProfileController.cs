c62027e49c7ad18cf208db7b46635afb1c172228
nicholasvperry <nicholasvperry@gmail.com>
2022-03-22 15:41:52 -0400

    added one note

--- Tabloid/Controllers/UserProfileController.cs
+++ Tabloid/Controllers/UserProfileController.cs
@@ -52,8 +52,13 @@ namespace Tabloid.Controllers
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
+
             var user = _userRepository.GetById(id);
 
+            if (user.ImageLocation == null)
+            {
+                user.ImageLocation = "https://avatars.dicebear.com/api/bottts/.svg";
+            }
             if (user == null)
             {
                 return NotFound();
